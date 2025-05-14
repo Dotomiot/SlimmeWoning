265fcc66d1b0676d0d1c55801761acbb490ca434
dotomiot <tom.dewever@telenet.be>
2025-05-14 10:19:15 +0200

    Added Lamp2

--- Classes/Apparaten.py
+++ Classes/Apparaten.py
@@ -1,3 +1,16 @@
 class Apparaat:
     def __init__(self):
         statusAan = False
+
+    def pasStatusAan(self, status):
+        self.statusAan = status
+
+class Lamp(Apparaat):
+    def __init__(self):
+        helderheid = 0
+
+    def veranderHelderheid(self, helderheid):
+        if helderheid > 100:
+            helderheid = 100
+        self.helderheid = helderheid
+    
