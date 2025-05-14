61dbc78290b1dbb975e81cfacdf1e9be3db07a44
dotomiot <tom.dewever@telenet.be>
2025-05-14 10:45:18 +0200

    Lamp Works

--- Classes/Apparaten.py
+++ Classes/Apparaten.py
@@ -1,16 +1,37 @@
 class Apparaat:
     def __init__(self):
-        statusAan = False
+        self.statusAan = False
 
     def pasStatusAan(self, status):
         self.statusAan = status
 
 class Lamp(Apparaat):
-    def __init__(self):
-        helderheid = 0
+    def __init__(self, helderheid=0):
+        super().__init__()
+        self.helderheid = helderheid
+
 
     def veranderHelderheid(self, helderheid):
         if helderheid > 100:
             helderheid = 100
         self.helderheid = helderheid
     
+    def __str__(self):
+        if self.statusAan == True:
+            return f"Mijn helderheid is {self.helderheid}% en ik sta aan"
+        else:
+            return f"Mijn helderheid is {self.helderheid}% en ik sta uit"
+    
+lama = Lamp(20)
+print(lama.helderheid)
+print(lama.statusAan)
+
+print(lama)
+
+lama.veranderHelderheid(69)
+lama.pasStatusAan(True)
+
+print(lama.helderheid)
+print(lama.statusAan)
+
+print(lama)
